677d9742cad06be2a9cfaf79b2d9eb31c104fd2d
njdeane <njdeane7@gmail.com>
2020-11-23 14:31:35 +1000

    secrets.ejs renders if email/password matches

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -10,6 +11,16 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const userSchema = {
+  email: String,
+  password: String
+};
+
+const User = new mongoose.model("user", userSchema);
+
 
 app.get("/", function(req, res){
   res.render("home.ejs");
@@ -23,6 +34,20 @@ app.get("/register", function(req, res){
   res.render("register.ejs");
 });
 
+app.post("/register", function(req, res){
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+
+  newUser.save(function(err){
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
+});
 
 
 
