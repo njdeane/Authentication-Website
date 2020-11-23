b9f2b0294cfef7f5af109a205e2dd0b14f8f4662
njdeane <njdeane7@gmail.com>
2020-11-23 14:14:09 +1000

    get routes done

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 //jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get("/", function(req, res){
+  res.render("home.ejs");
+});
+
+app.get("/login", function(req, res){
+  res.render("login.ejs");
+});
+
+app.get("/register", function(req, res){
+  res.render("register.ejs");
+});
+
+
+
+
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000");
+});
